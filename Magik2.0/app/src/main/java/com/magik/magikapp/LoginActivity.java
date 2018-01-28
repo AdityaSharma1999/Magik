2e9091da8d1bfc84e07c1b8708a122516f3d8643
Ansh Sikka <sikka008@umn.edu>
2018-01-28 06:26:42 -0600

    Sign Up Creator

--- Magik2.0/app/src/main/java/com/magik/magikapp/LoginActivity.java
+++ Magik2.0/app/src/main/java/com/magik/magikapp/LoginActivity.java
@@ -2,12 +2,34 @@ package com.magik.magikapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etxt_username;
+    private EditText etxt_password;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initElements();
+    }
+
+    private void initElements(){
+        etxt_username = (EditText) findViewById(R.id.et_username);
+        etxt_password = (EditText) findViewById(R.id.et_password);
+    }
+
+    private void getData(){
+        username = etxt_username.getText().toString();
+        password = etxt_password.getText().toString();
+    }
+
+    private void setListener(){
+
     }
 }
