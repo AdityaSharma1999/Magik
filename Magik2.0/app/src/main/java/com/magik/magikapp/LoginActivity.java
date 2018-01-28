0b5bf4606a1e704936e90099079482bdfa54f289
Ansh Sikka <sikka008@umn.edu>
2018-01-28 06:47:15 -0600

    Sign Up Creator

--- Magik2.0/app/src/main/java/com/magik/magikapp/LoginActivity.java
+++ Magik2.0/app/src/main/java/com/magik/magikapp/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.magik.magikapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText etxt_username;
     private EditText etxt_password;
+    private Button btn_login_main;
     private String username;
     private String password;
 
@@ -22,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     private void initElements(){
         etxt_username = (EditText) findViewById(R.id.et_username);
         etxt_password = (EditText) findViewById(R.id.et_password);
+        btn_login_main = (Button) findViewById(R.id.btn_login_main);
     }
 
     private void getData(){
@@ -30,6 +40,21 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void setListener(){
-
+        btn_login_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logInInBackground(username, password, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Intent goHome = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(goHome);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "We had trouble logging in. \n" + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
